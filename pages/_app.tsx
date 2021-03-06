3f51649804a96cdfb22e45f4b32b4e5c5c1d11e4
Itai Fish <chessandpotatoes@gmail.com>
2021-03-05 23:30:33 -0500

    working website from tutorial in typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../styles/global.css';
+import { AppProps } from 'next/app';
+import * as React from 'react';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+import { GetStaticProps } from 'next';
+import * as React from 'react';
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}): JSX.Element {
   return (
     <Layout home>
       <Head>
@@ -35,14 +45,14 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData
     }
-  }
-}
+  };
+};
